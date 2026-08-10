4abcee55b3c9867fcbe33ea983b1bf835936c7c1
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-08-10 20:30:20 +0300

    UPDATE

--- src/Arbiter.cpp
+++ src/Arbiter.cpp
@@ -128,11 +128,18 @@ void Arbiter::PreStep(float inv_dt)
 	}
 }
 
-Vec2 CalcRelativeVelocity(Body* b1, Body* b2, Vec2 r1, Vec2 r2)
+static inline Vec2 CalcRelativeVelocity(Body* b1, Body* b2, Vec2 r1, Vec2 r2)
 {
     return
     b2->velocity + Cross(b2->angularVelocity, r2) -
     b1->velocity - Cross(b1->angularVelocity, r1);
+}
+static inline void ApplyImpulse2(Body* b1, Body* b2, Vec2 r1, Vec2 r2, Vec2 impulse)
+{
+    b1->velocity -= b1->invMass * impulse;
+    b2->velocity += b2->invMass * impulse;
+    b1->angularVelocity -= b1->invI * Cross(r1, impulse);
+    b2->angularVelocity += b2->invI * Cross(r2, impulse);
 }
 
 void Arbiter::ApplyImpulse()
@@ -147,60 +154,36 @@ void Arbiter::ApplyImpulse()
 		Vec2 r1 = c->position - b1->position;
 		Vec2 r2 = c->position - b2->position;
 
-        Vec2 normal = c->normal;
-        Vec2 tangent = Cross(c->normal, 1.0f);
-
-        float dPn;
         {
             Vec2 vr = CalcRelativeVelocity(b1, b2, r1, r2);
-            dPn = c->massNormal * (-Dot(vr, normal) + c->bias);
 
-            if (World::accumulateImpulses)
-            {
-                float Pn_old = c->Pn;
-                float Pn_new = Max(c->Pn + dPn, 0.0f);
-                c->Pn = Pn_new;
+            Vec2 normal = c->normal;
+            float dPn = c->massNormal * (-Dot(vr, normal) + c->bias);
 
-                dPn = Pn_new - Pn_old;
-            }
-            else
-            {
-                dPn = Max(dPn, 0.0f);
-            }
+            float Pn_old = c->Pn;
+            float Pn_new = Max(c->Pn + dPn, 0.0f);
 
-            Vec2 Pn = dPn * normal;
-            b1->velocity -= b1->invMass * Pn;
-            b2->velocity += b2->invMass * Pn;
-            b1->angularVelocity -= b1->invI * Cross(r1, Pn);
-            b2->angularVelocity += b2->invI * Cross(r2, Pn);
+            c->Pn = Pn_new;
+            dPn = Pn_new - Pn_old;
+
+            ApplyImpulse2(b1, b2, r1, r2, dPn * normal);
         }
 
-        float dPt;
         {
             Vec2 vr = CalcRelativeVelocity(b1, b2, r1, r2);
-            dPt = c->massTangent * (-Dot(vr, tangent));
 
-            if (World::accumulateImpulses)
-            {
-                float Ptl_max = friction * c->Pn;
+            Vec2 tangent = Cross(c->normal, 1.0f);
+            float dPt = c->massTangent * -Dot(vr, tangent);
 
-                float Pt_old = c->Pt;
-                float Pt_new = Clamp(c->Pt + dPt, -Ptl_max, +Ptl_max);
-                c->Pt = Pt_new;
+            float Ptl_max = friction * c->Pn;
 
-                dPt = Pt_new - Pt_old;
-            }
-            else
-            {
-                float Ptl_max = friction * dPn;
-                dPt = Clamp(dPt, -Ptl_max, Ptl_max);
-            }
+            float Pt_old = c->Pt;
+            float Pt_new = Clamp(c->Pt + dPt, -Ptl_max, +Ptl_max);
 
-            Vec2 Pt = dPt * tangent;
-            b1->velocity -= b1->invMass * Pt;
-            b2->velocity += b2->invMass * Pt;
-            b1->angularVelocity -= b1->invI * Cross(r1, Pt);
-            b2->angularVelocity += b2->invI * Cross(r2, Pt);
+            c->Pt = Pt_new;
+            dPt = Pt_new - Pt_old;
+
+            ApplyImpulse2(b1, b2, r1, r2, dPt * tangent);
         }
 	}
 }
