11a0f052bd0fcf1765fafa831a05f3a269c7324a
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-08-12 11:49:30 +0300

    UPDATE

--- include/box2d-lite/World.h
+++ include/box2d-lite/World.h
@@ -22,24 +22,27 @@ struct Joint;
 
 struct World
 {
+public:
+	static bool accumulateImpulses;
+	static bool warmStarting;
+	static bool positionCorrection;
+
+	Vec2 gravity;
+	int iterations;
+
+    std::vector<Body*> bodies;
+	std::vector<Joint*> joints;
+	std::map<ArbiterKey, Arbiter> arbiters;
+
 	World(Vec2 gravity, int iterations) : gravity(gravity), iterations(iterations) {}
 
 	void Add(Body* body);
 	void Add(Joint* joint);
 	void Clear();
-
 	void Step(float dt);
 
+private:
 	void BroadPhase();
-
-	std::vector<Body*> bodies;
-	std::vector<Joint*> joints;
-	std::map<ArbiterKey, Arbiter> arbiters;
-	Vec2 gravity;
-	int iterations;
-	static bool accumulateImpulses;
-	static bool warmStarting;
-	static bool positionCorrection;
 };
 
 #endif
