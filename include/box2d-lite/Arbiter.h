7d91ba5487c4ef2bdda38d17e31b1494e9405bcb
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-08-17 21:18:52 +0300

    UPDATE

--- include/box2d-lite/Arbiter.h
+++ include/box2d-lite/Arbiter.h
@@ -1,16 +1,4 @@
-/*
-* Copyright (c) 2006-2009 Erin Catto http://www.gphysics.com
-*
-* Permission to use, copy, modify, distribute and sell this software
-* and its documentation for any purpose is hereby granted without fee,
-* provided that the above copyright notice appear in all copies.
-* Erin Catto makes no representations about the suitability
-* of this software for any purpose.
-* It is provided "as is" without express or implied warranty.
-*/
-
-#ifndef ARBITER_H
-#define ARBITER_H
+#pragma once
 
 #include "MathUtils.h"
 
@@ -96,5 +84,3 @@ inline bool operator < (const ArbiterKey& a1, const ArbiterKey& a2)
 }
 
 int Collide(Contact* contacts, Body* body1, Body* body2);
-
-#endif
