055f7202cda8fa79794b9e122dc7657a98cea8ed
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-08-17 15:20:28 +0300

    UPDATE

--- include/box2d-lite/MathUtils.h
+++ include/box2d-lite/MathUtils.h
@@ -1,16 +1,4 @@
-/*
-* Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com
-*
-* Permission to use, copy, modify, distribute and sell this software
-* and its documentation for any purpose is hereby granted without fee,
-* provided that the above copyright notice appear in all copies.
-* Erin Catto makes no representations about the suitability
-* of this software for any purpose.
-* It is provided "as is" without express or implied warranty.
-*/
-
-#ifndef MATHUTILS_H
-#define MATHUTILS_H
+#pragma once
 
 #include <math.h>
 #include <float.h>
@@ -297,6 +285,3 @@ inline Vec2 ShortPathToSurface(Vec2 point, Vec2 boxPosition, float boxRotation, 
 
     return result;
 }
-
-#endif
-
