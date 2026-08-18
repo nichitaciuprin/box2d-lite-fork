fab0db5cade3fae26cd29fa5fda8c9e7aa601a19
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-08-19 02:54:53 +0300

    UPDATE

--- include/box2d-lite/MathUtils.h
+++ include/box2d-lite/MathUtils.h
@@ -152,6 +152,7 @@ inline Vec2 operator * (const Vec2& v, float s)
 {
     return Vec2(v.x * s, v.y * s);
 }
+
 inline Mat22 operator + (const Mat22& A, const Mat22& B)
 {
     return Mat22(A.col1 + B.col1, A.col2 + B.col2);
