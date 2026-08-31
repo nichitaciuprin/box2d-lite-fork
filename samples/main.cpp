5e573d4a41c10c1704b3201d3b705ec6ddf88a04
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-08-31 17:54:13 +0300

    UPDATE

--- samples/main.cpp
+++ samples/main.cpp
@@ -11,8 +11,9 @@
 
 #include "box2d-lite/MathUtils.h"
 
-#include "box2d-lite/Arbiter.h"
 #include "box2d-lite/Body.h"
+
+#include "box2d-lite/Arbiter.h"
 #include "box2d-lite/Joint.h"
 
 #include "box2d-lite/World.h"
