72d65059d48ed95c7b5abbc1c447865fde729381
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-08-31 17:50:38 +0300

    UPDATE

--- include/box2d-lite/Joint.h
+++ include/box2d-lite/Joint.h
@@ -1,16 +1,4 @@
-/*
-* Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com
-*
-* Permission to use, copy, modify, distribute and sell this software
-* and its documentation for any purpose is hereby granted without fee,
-* provided that the above copyright notice appear in all copies.
-* Erin Catto makes no representations about the suitability
-* of this software for any purpose.
-* It is provided "as is" without express or implied warranty.
-*/
-
-#ifndef JOINT_H
-#define JOINT_H
+#pragma once
 
 #include "MathUtils.h"
 
@@ -39,5 +27,3 @@ struct Joint
     float biasFactor;
     float softness;
 };
-
-#endif
