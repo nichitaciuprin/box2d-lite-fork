75cecff3d14e229391f08a6818bcf1ee110efd50
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-09-01 06:32:58 +0300

    UPDATE

--- include/Body.h
+++ include/Body.h
@@ -36,67 +36,39 @@ struct Body
         inertiaInv = 0.0f;
     }
 
-    void Set(Vec2 scale_, float mass_)
-    {
-        position = { 0.0f, 0.0f };
-        rotation = 0.0f;
-        scale = scale_;
-
-        velocityLinear = { 0.0f, 0.0f };
-        velocityAngular = 0.0f;
-
-        force = { 0.0f, 0.0f };
-        torque = 0.0f;
-
-        friction = 0.2f;
-
-        if (mass_ == FLT_MAX)
-        {
-            mass = FLT_MAX;
-            massInv = 0.0f;
-            inertia = FLT_MAX;
-            inertiaInv = 0.0f;
-            return;
-        }
-
-        mass = mass_;
-        massInv = 1.0f / mass;
-        inertia = mass * (scale.x * scale.x + scale.y * scale.y) / 12.0f;
-        inertiaInv = 1.0f / inertia;
-    }
     void AddForce(Vec2 force_)
     {
         force += force_;
     }
 };
 
-void BodySet(Body& body, Vec2 scale_, float mass_)
+void BodySet(Body* body, Vec2 scale_, float mass_)
 {
-    body.position = { 0.0f, 0.0f };
-    body.rotation = 0.0f;
-    body.scale = scale_;
+    body->position = { 0.0f, 0.0f };
+    body->rotation = 0.0f;
+    body->scale = scale_;
 
-    body.velocityLinear = { 0.0f, 0.0f };
-    body.velocityAngular = 0.0f;
+    body->velocityLinear = { 0.0f, 0.0f };
+    body->velocityAngular = 0.0f;
 
-    body.force = { 0.0f, 0.0f };
-    body.torque = 0.0f;
+    body->force = { 0.0f, 0.0f };
+    body->torque = 0.0f;
 
-    body.friction = 0.2f;
+    body->friction = 0.2f;
 
     if (mass_ == FLT_MAX)
     {
-        body.mass = FLT_MAX;
-        body.massInv = 0.0f;
-        body.inertia = FLT_MAX;
-        body.inertiaInv = 0.0f;
+        body->mass = FLT_MAX;
+        body->massInv = 0.0f;
+        body->inertia = FLT_MAX;
+        body->inertiaInv = 0.0f;
         return;
     }
 
-    body.mass = mass_;
-    body.massInv = 1.0f / body.mass;
-    body.inertia = body.mass * (body.scale.x * body.scale.x + body.scale.y * body.scale.y) / 12.0f;
-    body.inertiaInv = 1.0f / body.inertia;
+    body->mass = mass_;
+    body->massInv = 1.0f / body->mass;
+    body->inertia = body->mass * (body->scale.x * body->scale.x + body->scale.y * body->scale.y) / 12.0f;
+    body->inertiaInv = 1.0f / body->inertia;
 }
 void BodyAddForce(Body& body, Vec2 force_)
 {
