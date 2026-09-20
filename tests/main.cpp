d6e8c5be678225f86033fc0330ab52eb80b3bf9f
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-09-20 10:14:02 +0300

    UPDATE

--- tests/main.cpp
+++ tests/main.cpp
@@ -1082,9 +1082,9 @@ void Draw()
         DrawText(5, 35, "Keys: 1-9 Demos, Space to Launch the Bomb");
 
         char buffer[512];
-        sprintf(buffer, "(A) Accumulation %s",        Config::accumulateImpulses ? "ON" : "OFF"); DrawText(5, 65,  buffer);
-        sprintf(buffer, "(S) Position Correction %s", Config::positionCorrection ? "ON" : "OFF"); DrawText(5, 95,  buffer);
-        sprintf(buffer, "(D) Warm Starting %s",       Config::warmStarting       ? "ON" : "OFF"); DrawText(5, 125, buffer);
+        sprintf(buffer, "(A) Accumulation %s",        Config::accumulateImpulses ? "ON" : "OFF"); DrawText(5, 65 + 30*0, buffer);
+        sprintf(buffer, "(S) Position Correction %s", Config::positionCorrection ? "ON" : "OFF"); DrawText(5, 65 + 30*1, buffer);
+        sprintf(buffer, "(D) Warm Starting %s",       Config::warmStarting       ? "ON" : "OFF"); DrawText(5, 65 + 30*2, buffer);
     }
     GuiEnd();
 }
