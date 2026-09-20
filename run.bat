5bc84e44346f35ededa2b6b0a1479906857a3859
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-09-20 07:07:02 +0300

    UPDATE

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 setlocal
 
-echo y | gdb -q -ex=run -ex=backtrace -ex=quit --args %cd%\build\samples\samples.exe
+echo y | gdb -q -ex=run -ex=backtrace -ex=quit --args %cd%\build\tests\Test1.exe
