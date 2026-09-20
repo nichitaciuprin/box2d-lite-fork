d4ee1b3aaed524854ceba7114bdcf496da47fe17
Nichita Ciuprin <nichitaciuprin@gmail.com>
2026-09-20 07:13:18 +0300

    UPDATE

---
+++ base/include/Core.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl2.h"
+
+#include <stdio.h>
+#include <iostream>
+
+#include <vector>
+#include <map>
+
+using namespace std;
+
+#include "MathUtils.h"
+#include "Config.h"
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,20 +1,4 @@
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
-
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl2.h"
-
-#include <stdio.h>
-#include <iostream>
-
-#include <vector>
-#include <map>
-
-using namespace std;
-
-#include "MathUtils.h"
-#include "Config.h"
+#include "Core.h"
 
 struct Body
 {
